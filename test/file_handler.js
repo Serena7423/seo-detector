64b0cad8825e2507435c056b0da34972a77a14e8
Serena <serena_cheng@serenas-MacBook-Pro.local>
2018-10-22 22:44:29 +0800

    Define constant variable for path and contents.

--- test/file_handler.js
+++ test/file_handler.js
@@ -7,16 +7,19 @@ const async = require('asyncawait/async');
 const await = require('asyncawait/await');
 const fileHandler = require('../lib/file_handler');
 
+const errorPath = '../static/error.txt';
+
 describe('File', function() {
     describe('#read file', function() {
         it('should read file test.txt without error', async(function() {
+            const content = 'mocha_read_testing';
             const result = await(fileHandler.read(global.__BASE__ + 'test/test.txt'));
-            assert.equal(result, "hello");
+            assert.equal(result, content);
         }))
 
         it('should read with error for invalid path', async(function() {
             try {
-                await(fileHandler.read('../static/error.txt'));
+                await(fileHandler.read(errorPath));
             } catch(err) {
                 assert(err instanceof Error);
                 assert.equal(err.code, 'ENOENT');
@@ -24,8 +27,8 @@ describe('File', function() {
         }))
     })
     describe('#write file', function() {
-        it('should write file to out.txt without error', async(function() {
-            const content = 'mocha_write_testing';
+        const content = 'mocha_write_testing';
+        it('should write file to out.txt without error', async(function() { 
             const path = global.__BASE__ + 'test/out.txt';
             await(fileHandler.write(path, content));
             const result = await(fileHandler.read(path));
@@ -34,7 +37,7 @@ describe('File', function() {
 
         it('should write file to error.txt with error for invalid path', async(function() {
             try {
-                await(fileHandler.write('../static/error.txt', 'mocha_write_testing'));
+                await(fileHandler.write(errorPath, content));
             } catch(err) {
                 assert(err instanceof Error);
                 assert.equal(err.code, 'ENOENT');
